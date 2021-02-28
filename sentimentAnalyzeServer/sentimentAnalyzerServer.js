cdca617bd551006e16e5221b28896f43d5241872
nickbulm <nicholas.bulmer93@gmail.com>
2021-03-01 07:34:02 +1030

    Some Updates

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const dotenv = require('dovenv');
 const app = new express();
+
+dotenv.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NAturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: ' 2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
 
 app.use(express.static('client'))
 
 const cors_app = require('cors');
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 app.use(cors_app());
 
 app.get("/",(req,res)=>{
@@ -11,7 +32,7 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-
+    
     return res.send({"happy":"90","sad":"10"});
 });
 
